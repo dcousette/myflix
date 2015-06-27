095c3b93b822832d283d0585b6e316f48fdbf95a
DeShawn <dcousette@gmail.com>
2015-06-27 22:34:22 +0000

    Add recent_videos method and test

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,4 +2,64 @@ require 'spec_helper'
 
 describe Category do
   it { should have_many(:videos)}
+  
+  describe '#recent_videos' do
+    it 'returns the videos in reverse chronical order by created_at' do 
+      tv_shows = Category.create(name:'TV Shows')
+      
+      futurama = Video.create(title:"Futurama", 
+                              description:"It's about the Future, duh!",
+                              category: tv_shows,
+                              created_at: 1.day.ago)
+      
+      back_to_future = Video.create(title:"Back to the Future", 
+                              description:"Michael J Fox!",
+                              category: tv_shows)
+      
+      expect(tv_shows.recent_videos).to eq([back_to_future, futurama])
+    end
+    
+    it 'returns all of the videos if there are less than 6 videos' do 
+      tv_shows = Category.create(name:'TV Shows')
+      
+      futurama = Video.create(title:"Futurama", 
+                              description:"It's about the Future, duh!",
+                              category: tv_shows)
+      
+      back_to_future = Video.create(title:"Back to the Future", 
+                              description:"Michael J Fox!",
+                              category: tv_shows)
+      
+      expect(tv_shows.recent_videos.count).to eq(2)
+    end 
+    
+    it 'returns 6 videos if there are more than 6 videos' do 
+      tv_shows = Category.create(name:'TV Shows')
+      
+      7.times { Video.create(title:"Test Movie", description:"Justing testing things", 
+                             category: tv_shows)}
+      
+      expect(tv_shows.recent_videos.count).to eq(6)  
+    end
+    
+    it 'returns the most recent 6 videos' do 
+      tv_shows = Category.create(name:'TV Shows')
+      
+      futurama = Video.create(title:"Futurama", 
+                              description:"It's about the Future, duh!",
+                              category: tv_shows,
+                              created_at: 1.day.ago)
+      
+      7.times { Video.create(title:"Test Movie", description:"Justing testing things", 
+                             category: tv_shows)}
+      
+      expect(tv_shows.recent_videos).not_to include(futurama) 
+    end
+    
+    it "returns an empty array if the category doesn't have any videos" do 
+      tv_shows = Category.create(name:'TV Shows')
+      
+      expect(tv_shows.recent_videos).to eq([])
+    end
+  end
 end
