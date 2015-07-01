9ec75b336df12703dac1ef6cc39264873dd361dc
DeShawn <dcousette@gmail.com>
2015-07-01 19:05:09 +0000

    Add authentication

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Myflix::Application.routes.draw do
   get  'register', to: 'users#new'
   get  'signin', to: 'sessions#new'
   post 'signin', to: 'sessions#create'
+  delete  'signout', to: 'sessions#destroy'
   
   root to: 'static_pages#home'
   
