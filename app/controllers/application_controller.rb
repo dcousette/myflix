b2872dca7b5fad971984a0404ba9615b3e045d04
DeShawn <dcousette@gmail.com>
2015-07-12 19:43:13 +0000

    Add tests for authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  helper_method :logged_in?
+  helper_method :logged_in?, :current_user
   
   def current_user 
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,15 @@
 class SessionsController < ApplicationController
+  def new 
+    redirect_to home_path if current_user   
+  end
+  
   def create
     user = User.find_by(email_address: params[:email_address])
     
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       flash[:success] = "You are logged in"
-      redirect_to videos_path
+      redirect_to home_path
     else
       flash[:danger] = 'Please enter the correct username and password'
       render :new
