8a758d35cbb1b215e4470ce504a95522c23dd16d
DeShawn <dcousette@gmail.com>
2015-08-20 04:36:31 +0000

    Add feature spec for queue interaction

--- spec/support/macros.rb
+++ spec/support/macros.rb
@@ -1,3 +1,12 @@
 def setup_current_user(user=nil)
   session[:user_id] = (user || Fabricate(:user)).id 
 end
+
+def sign_in(a_user=nil)
+  user = a_user || Fabricate(:user)
+  visit signin_path 
+  fill_in "Email Address", with: user.email_address 
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+end
+
