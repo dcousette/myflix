da9b7e4548c36aa7753619d75f751a927b88ae18
DeShawn <dcousette@gmail.com>
2015-10-26 04:42:30 +0000

    Edit seeds db file

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,55 +13,54 @@ reality_tv  = Category.create(name:"Reality TV")
 futurama = Video.create(title:'Futurama', 
              description:'A funny cartoon show set in the future.', 
              small_cover_url:'futurama.jpg', 
-             large_cover_url:'futurama.jpg',
+             large_cover_url:'monk_large.jpg',
              category: tv_dramas)
 
 Video.create(title:'Family Guy', 
              description:'A cartoon about a baby named Stuey', 
              small_cover_url:'family_guy.jpg', 
-             large_cover_url:'family_guy.jpg',
+             large_cover_url:'monk_large.jpg',
              category: tv_comedies)
 
 Video.create(title:'South Park', 
              description:'A cartoon about some bad little kids.', 
              small_cover_url:'south_park.jpg', 
-             large_cover_url:'south_park.jpg',
+             large_cover_url:'monk_large.jpg',
              category: reality_tv)
 
 Video.create(title:'Family Guy', 
              description:'A cartoon about a baby named Stuey', 
              small_cover_url:'family_guy.jpg', 
-             large_cover_url:'family_guy.jpg',
+             large_cover_url:'monk_large.jpg',
              category: tv_comedies)
    
 Video.create(title:'Family Guy', 
              description:'A cartoon about a baby named Stuey', 
              small_cover_url:'family_guy.jpg', 
-             large_cover_url:'family_guy.jpg',
+             large_cover_url:'monk_large.jpg',
              category: tv_comedies)
              
 Video.create(title:'Family Guy', 
              description:'A cartoon about a baby named Stuey', 
              small_cover_url:'family_guy.jpg', 
-             large_cover_url:'family_guy.jpg',
+             large_cover_url:'monk_large.jpg',
              category: tv_comedies)
 
 Video.create(title:'Family Guy', 
              description:'A cartoon about a baby named Stuey', 
              small_cover_url:'family_guy.jpg', 
-             large_cover_url:'family_guy.jpg',
+             large_cover_url:'monk_large.jpg',
+             category: tv_comedies)   
+Video.create(title:'Family Guy', 
+             description:'A cartoon about a baby named Stuey', 
+             small_cover_url:'family_guy.jpg', 
+             large_cover_url:'monk_large.jpg',
              category: tv_comedies)
              
 Video.create(title:'Family Guy', 
              description:'A cartoon about a baby named Stuey', 
              small_cover_url:'family_guy.jpg', 
-             large_cover_url:'family_guy.jpg',
-             category: tv_comedies)
-
-Video.create(title:'Family Guy', 
-             description:'A cartoon about a baby named Stuey', 
-             small_cover_url:'family_guy.jpg', 
-             large_cover_url:'family_guy.jpg',
+             large_cover_url:'monk_large.jpg',
              category: tv_comedies)
              
 deshawn = User.create(email_address: 'dcousette@gmail.com', password: 'dcousette', full_name: 'DeShawn Cousette')
