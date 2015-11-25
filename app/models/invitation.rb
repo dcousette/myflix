5cc4d2549b831534db371b023e0405e1dfefddc6
DeShawn Cousette <dcousette@gmail.com>
2015-11-25 17:45:27 -0500

    correct error in invitation model

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -3,8 +3,4 @@ class Invitation < ActiveRecord::Base
   belongs_to :inviter, class_name: "User"
   validates_presence_of :recipient_name, :recipient_email, :message
   before_create :generate_token
-
-  it_behaves_like "tokenable" do
-    let(:object) { Fabricate(:invitation) }
-  end
 end
