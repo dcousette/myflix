22caa76c19174530a0d49fecd78999ed78c04833
DeShawn Cousette <dcousette@gmail.com>
2015-12-16 15:38:53 -0500

    Add Amazon S3 configuration

--- spec/fabricators/video_fabricator.rb
+++ spec/fabricators/video_fabricator.rb
@@ -2,6 +2,6 @@ Fabricator(:video) do
   title {Faker::Lorem.words(5).join(" ")}
   description {Faker::Lorem.paragraph(2)}
   category
-  small_cover_url "file1"
-  large_cover_url "file2"
+  small_cover "file1"
+  large_cover "file2"
 end
