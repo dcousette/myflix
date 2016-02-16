a9f999470763b5418fbfbde5586fbc832c9fccc5
DeShawn Cousette <dcousette@gmail.com>
2016-02-16 14:23:39 -0500

    add a decorator for video using draper gem

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,62 +1,62 @@
 require 'spec_helper'
 
-describe Video do 
+describe Video do
   it { should belong_to(:category)}
   it { should have_many(:reviews)}
   it { should validate_presence_of(:title)}
   it { should validate_presence_of(:description)}
   it { should have_many(:reviews).order('created_at DESC') }
-  
-  describe "search_by_title" do 
-    it 'returns an empty array if there is no match' do 
-      futurama = Video.create(title:"Futurama", 
+
+  describe "search_by_title" do
+    it 'returns an empty array if there is no match' do
+      futurama = Video.create(title:"Futurama",
                               description:"It's about the Future, duh!")
-      
-      back_to_future = Video.create(title:"Back to the Future", 
+
+      back_to_future = Video.create(title:"Back to the Future",
                               description:"Michael J Fox!")
-                              
+
       expect(Video.search_by_title("Pacman")).to eq([])
     end
-    
-    it 'returns an array of one video for an exact match' do 
-      futurama = Video.create(title:"Futurama", 
+
+    it 'returns an array of one video for an exact match' do
+      futurama = Video.create(title:"Futurama",
                               description:"It's about the Future, duh!")
-      
-      back_to_future = Video.create(title:"Back to the Future", 
+
+      back_to_future = Video.create(title:"Back to the Future",
                               description:"Michael J Fox!")
-      
+
       expect(Video.search_by_title("Futurama")).to eq([futurama])
     end
-    
-    it 'returns an array of one video for a partial match' do 
-      futurama = Video.create(title:"Futurama", 
+
+    it 'returns an array of one video for a partial match' do
+      futurama = Video.create(title:"Futurama",
                               description:"It's about the Future, duh!")
-      
-      back_to_future = Video.create(title:"Back to the Future", 
+
+      back_to_future = Video.create(title:"Back to the Future",
                               description:"Michael J Fox!")
-      
+
       expect(Video.search_by_title("urama")).to eq([futurama])
     end
-    
-    it 'returns an array of all matches ordered by created_at' do 
-      futurama = Video.create(title:"Futurama", 
+
+    it 'returns an array of all matches ordered by created_at' do
+      futurama = Video.create(title:"Futurama",
                               description:"It's about the Future, duh!",
                               created_at: 1.day.ago)
-      
-      back_to_future = Video.create(title:"Back to the Future", 
+
+      back_to_future = Video.create(title:"Back to the Future",
                               description:"Michael J Fox!")
-      
+
       expect(Video.search_by_title("Futur")).to eq([back_to_future, futurama])
-    end 
-    
-    it 'returns an empty array for a search with an empty string' do 
-      futurama = Video.create(title:"Futurama", 
+    end
+
+    it 'returns an empty array for a search with an empty string' do
+      futurama = Video.create(title:"Futurama",
                               description:"It's about the Future, duh!",
                               created_at: 1.day.ago)
-      
-      back_to_future = Video.create(title:"Back to the Future", 
+
+      back_to_future = Video.create(title:"Back to the Future",
                               description:"Michael J Fox!")
-      
+
       expect(Video.search_by_title("")).to eq([])
     end
   end
